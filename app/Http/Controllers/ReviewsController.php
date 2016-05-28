ed28eaea21784d6fa4301060d291ea366c09a3a5
Agu Dondo <agustind@gmail.com>
2016-05-28 11:47:21 -0300

    multiple ratings for reviews

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -2,107 +2,111 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\Page;
+use App\Photo;
 use App\Review;
 use App\User;
-use App\Page;
+use Illuminate\Http\Request;
 use Image;
-use App\Photo;
 
-class ReviewsController extends Controller
-{
+class ReviewsController extends Controller {
 
-    public function index(){    	
-    	$reviews = Review::with('photos')->get();
-        foreach($reviews as $index => $review){
-            $reviews[$index]->place = $review->page->title;
-        }
-        return $reviews;
-    }
+	public function index() {
+		$reviews = Review::with('photos')->get();
+		foreach ($reviews as $index => $review) {
+			$reviews[$index]->place = $review->page->title;
+		}
+		return $reviews;
+	}
 
-    public function show(Review $review){
-        $review->date = date('d/m/Y', strtotime($review->created_at));
-        $review->photos = $review->photos()->get();
-        $review->pictures = $review->getImages();
-        $review->thumb = $review->getThumb();
-        $review->user = $review->user()->get();
-    	return $review;
-    }
+	public function show(Review $review) {
+		$review->date = date('d/m/Y', strtotime($review->created_at));
+		$review->photos = $review->photos()->get();
+		$review->pictures = $review->getImages();
+		$review->thumb = $review->getThumb();
+		$review->user = $review->user()->get();
+		return $review;
+	}
 
-    public function update(Review $review){
-        $request = request()->all();
-        $review->title = $request['title'];
-        $review->content = $request['content'];
-        $review->rating = $request['rating'];
-        $review->save();
-        return response()->json(['status' => 'success']);
-    }
+	public function update(Review $review) {
+		$request = request()->all();
+		$review->title = $request['title'];
+		$review->content = $request['content'];
+		$review->rating = $request['rating'];
+		$review->save();
+		return response()->json(['status' => 'success']);
+	}
 
-    public function delete(Review $review){
-        $review->delete();
-        return response()->json(['status' => 'success']);
-    }
+	public function delete(Review $review) {
+		$review->delete();
+		return response()->json(['status' => 'success']);
+	}
 
-    public function userReviews(User $user){
-        $reviews = $user->reviews()->with('photos')->get();
-        foreach($reviews as $index => $review){
-            $reviews[$index]->thumb = $review->getThumb();
-            $reviews[$index]->place = '';
-            if(count($review->page))$reviews[$index]->place = $review->page->title;
-        }
-        return $reviews;
-    }
+	public function userReviews(User $user) {
+		$reviews = $user->reviews()->with('photos')->get();
+		foreach ($reviews as $index => $review) {
+			$reviews[$index]->thumb = $review->getThumb();
+			$reviews[$index]->place = '';
+			if (count($review->page)) {
+				$reviews[$index]->place = $review->page->title;
+			}
 
-    public function pageReviews(Page $page){
-    	$reviews = $page->reviews()->with('photos')->get();
-        foreach($reviews as $index => $review){
-            $reviews[$index]->thumb = $review->getThumb();
-            $reviews[$index]->place = '';
-            if(count($review->page))$reviews[$index]->place = $review->page->title;
-        }
-        return $reviews;
-    }
+		}
+		return $reviews;
+	}
 
+	public function pageReviews(Page $page) {
+		$reviews = $page->reviews()->with('photos')->get();
+		foreach ($reviews as $index => $review) {
+			$reviews[$index]->thumb = $review->getThumb();
+			$reviews[$index]->place = '';
+			if (count($review->page)) {
+				$reviews[$index]->place = $review->page->title;
+			}
 
+		}
+		return $reviews;
+	}
 
-    public function store(User $user){
-        $request = request()->all();
-        $page = Page::find($request['page_id']);
-        
-        error_log(print_r($request,1));
+	public function store(User $user) {
+		$request = request()->all();
+		$page = Page::find($request['page_id']);
 
-        $review = new Review;
-        $review->title = $request['title'];
-        $review->content = $request['content'];
-        $review->rating = $request['rating'];
-        
-        $user->reviews()->save($review);
-        $page->reviews()->save($review);
+		error_log(print_r($request, 1));
 
-        return response()->json(['status' => 'success', 'review_id' => $review->id]);
+		$review = new Review;
+		$review->title = $request['title'];
+		$review->content = $request['content'];
+		$review->rating_0 = $request['rating'][0];
+		$review->rating_1 = $request['rating'][1];
+		$review->rating_2 = $request['rating'][2];
+		$review->rating_3 = $request['rating'][3];
 
-    }
+		$user->reviews()->save($review);
+		$page->reviews()->save($review);
 
+		return response()->json(['status' => 'success', 'review_id' => $review->id]);
 
-    public function addImage(Review $review){
-        
-        $request = request()->all();
+	}
 
-        // create the photo record
-        $photo = new Photo;
-        $photo->review_id = $review->id;
-        $photo->save();
+	public function addImage(Review $review) {
 
-        // upload photo and generate thumbs
-        if(isset($request['file'])){
-            $request['file']->move('photos', $photo->id . '.jpg');
-            // generate thumbs
-            Image::make('photos/' . $photo->id . '.jpg')->fit(1000, 1000)->save('photos/' . $photo->id . '.jpg')->fit(160,160)->save('photos/' . $photo->id . '_thumb.jpg');
-        }
-       
-        return response()->json(['status' => 'success']);
+		$request = request()->all();
 
-    }
+		// create the photo record
+		$photo = new Photo;
+		$photo->review_id = $review->id;
+		$photo->save();
+
+		// upload photo and generate thumbs
+		if (isset($request['file'])) {
+			$request['file']->move('photos', $photo->id . '.jpg');
+			// generate thumbs
+			Image::make('photos/' . $photo->id . '.jpg')->fit(1000, 1000)->save('photos/' . $photo->id . '.jpg')->fit(160, 160)->save('photos/' . $photo->id . '_thumb.jpg');
+		}
+
+		return response()->json(['status' => 'success']);
+
+	}
 
 }
