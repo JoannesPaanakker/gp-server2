942a49f70f4fb9eaed6a1bd7994f0ea8e39600bd
Agu Dondo <agustind@gmail.com>
2016-07-26 09:00:54 -0300

    debug

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -39,13 +39,16 @@ class QuizController extends Controller
         foreach ($questions as $question) {
             if (array_key_exists($question->id, $request['quiz_answers'])) {
                 $answer_text = '';
+                $answer_score = 0;
                 foreach ($question->answers as $selected_answer) {
                     $answer_text = $selected_answer->answer;
+                    $answer_score = $selected_answer->score;
                 }
                 $answer = new QuizAnswer;
                 $answer->user_id = $user->id;
                 $answer->question_text = $question->question;
                 $answer->answer = $answer_text;
+                $answer->score = $answer_score;
                 $answer->save();
             }
         }
