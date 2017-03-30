267a11b6f162be97c68c829a70d88a1f334fa3af
Agustin <agustind@gmail.com>
2017-03-30 09:47:03 -0300

    social links on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
 					<div class="landing-content-copy">
 						<h2>GreenPlatform helps people and companies live a greener life in a fun and social way.</h2>
 						<p>GreenPlatform offers green advice, products and tips on green company pages. You can search your locality on the map to find green cafes, restaurants, products and companies. You can read or even post your own green reviews. There will prizes for the best green shops and products.</p>
+						<br>
+						<div id="share" class="share-home"></div>
 					</div>
 				</div>
 			</div>
