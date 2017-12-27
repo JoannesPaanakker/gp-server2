3b92c24a7666b57dd5186b12af57b69639c24023
Agu <agustind@gmail.com>
2017-12-27 11:20:35 +0100

    links fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 						<a href="https://www.facebook.com/greenplatform.org/?fref=ts" target="_blank" class="social-link"><i class="fa fa-facebook"></i></a>
 						<br>
 						<a href="https://itunes.apple.com/app/greenplatform/id1170382180" target="_blank"><img src="/img/download-ios.png" alt="" class="download-button-ios"></a>
-						<a href="https://play.google.com/store/apps/details?id=org.greenplatform.gp" target="_blank"><img src="/img/download-android.png" alt="" class="download-button-android"></a>
+						<a href="https://play.google.com/store/apps/details?id=org.greenplatform.gpandroid&hl=es" target="_blank"><img src="/img/download-android.png" alt="" class="download-button-android"></a>
 					</div>
 				</div>
 			</div>
