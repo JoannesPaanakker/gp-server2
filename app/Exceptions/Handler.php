57bea6552403b12b98a9edb683241b1bcf25b04f
Agu <agustind@gmail.com>
2018-02-13 23:05:18 -0300

    debug

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,7 +45,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return response()->json($e);
-        return parent::render($request, $e);
+        
+        $json = [
+            'success' => false,
+            'error' => [
+                'code' => $e->getCode(),
+                'message' => $e->getMessage(),
+            ],
+        ];
+
+        return response()->json($json, 500);
+    
     }
 }
