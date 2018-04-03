5b039794783f16cc43a2f6fecd8aa92f61c6e652
Agu <agustind@gmail.com>
2018-04-03 16:08:15 -0300

    debug

--- app/Http/Helpers/GPHelper.php
+++ app/Http/Helpers/GPHelper.php
@@ -12,7 +12,7 @@ class GPHelper
     public static function googleSearch($position, $query = '', $type = 'any')
     {
         $radius = 600; // in metres
-        $rankby = '';
+        $rankby = 'distance';
 
         $query = urlencode($query);
 
